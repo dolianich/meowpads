2e769f528cb93bfbf3085ed0d70d11a9f97e02c5
dolianich <69437697+dolianich@users.noreply.github.com>
2022-12-29 20:51:06 +0300

    Add keyboard

--- index.js
+++ index.js
@@ -6,51 +6,58 @@ for (var i = 0; i<numberOfDrumButtons; i++){
     document.querySelectorAll(".meow")[i].addEventListener("click", function(){
        
         var buttonInnerHTML = this.innerHTML;
-        
-        switch (buttonInnerHTML) {
-            case "w":
-                var meow1 = new Audio("sounds/meow1.mp3");
-                meow1.play();
-                
-                break;
-            case "a":
-                var meow2 = new Audio("sounds/meow2.mp3");
-                meow2.play();
-            break; 
-            
-            case "s":
-                var meow3 = new Audio("sounds/meow3.mp3");
-                meow3.play();
-            break; 
-            
-            case "d":
-                var meow4 = new Audio("sounds/meow2.mp3");
-                meow4.play();
-            break; 
-            
-            case "j":
-                var meow5 = new Audio("sounds/meow2.mp3");
-                meow5.play();
-            break;  
-
-            case "k":
-                var meow6 = new Audio("sounds/meow2.mp3");
-                meow6.play();
-            break;  
-
-            case "l":
-                var meow7 = new Audio("sounds/meow2.mp3");
-                meow7.play();
-            break;  
-        
-        
-        
-        
-        
-        
-            default: console.log(buttonInnerHTML);
-                break;
-        }
+        makeSound(buttonInnerHTML);
     })
 }
 
+
+addEventListener("keydown", function(event){
+    makeSound(event.key);
+});
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var meow1 = new Audio("sounds/meow1.mp3");
+            meow1.play();
+            
+            break;
+        case "a":
+            var meow2 = new Audio("sounds/meow2.mp3");
+            meow2.play();
+        break; 
+        
+        case "s":
+            var meow3 = new Audio("sounds/meow3.mp3");
+            meow3.play();
+        break; 
+        
+        case "d":
+            var meow4 = new Audio("sounds/meow2.mp3");
+            meow4.play();
+        break; 
+        
+        case "j":
+            var meow5 = new Audio("sounds/meow2.mp3");
+            meow5.play();
+        break;  
+
+        case "k":
+            var meow6 = new Audio("sounds/meow2.mp3");
+            meow6.play();
+        break;  
+
+        case "l":
+            var meow7 = new Audio("sounds/meow2.mp3");
+            meow7.play();
+        break;  
+    
+    
+    
+    
+    
+    
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+}
